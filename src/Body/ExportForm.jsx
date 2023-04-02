e0808057659cd98a98f78e10d6a71a8f75977d19
Anton Kondratiev <antony.kondratiev@gmail.com>
2023-04-02 22:21:03 +0300

    homework edited 2

--- src/Body/ExportForm.jsx
+++ src/Body/ExportForm.jsx
@@ -16,24 +16,20 @@ function ExportForm() {
       },          
     ];
 
-  const ExportClick= () => {
-        window.open();
-  };
-
-  const handleSubmit = (event) => {
-      event.preventDefault();}
+   const handleSubmit = (event) => {
+       window.open();}
 
  return (        
          
     <Form onSubmit={handleSubmit}>
     <Form.Group className="mb-3">
     <Form.Label>What type of data do you want to export? </Form.Label>
-    <Form.Select aria-label="Chose format" name='format' onChange={handleSubmit}>
+    <Form.Select aria-label="Chose format" name='format'>
        {formatValues.map(({label}) => (<option value={label} key={label}>{label}</option>))}
     </Form.Select>
    </Form.Group>
         <div className="d-grid gap-2">
-        <Button variant="primary" type="submit" onClick={ExportClick}>
+        <Button variant="primary" type="submit" onChange={handleSubmit}>
           Export
         </Button>
       </div>
