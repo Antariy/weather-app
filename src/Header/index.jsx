d0d3ab036894f5db2c726a8df119a85c3e4b4682
Antariy <antony.kondratiev@gmail.com>
2023-04-27 16:58:14 +0300

    homework NavDropdown

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -4,46 +4,51 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import moment from "moment/moment";
-
+import { useState } from 'react';
 
 
 function Header({ list }) {
-  console.log(list);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  const getUrl = (el) => {
-    const url = `/forecast/${el}`;
-    return url;
-  }; 
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
 
-    return (
+  };
+  
+  return (
     <Navbar bg="primary" variant="dark" expand="sm">
       <Container>
         <Link to="/" className='navbar-brand'>
-        <img
-              alt=""
-              src="/logo.png"
-              width="50"
-              height="50"
-              className="d-inline-block align-center"
-              />&nbsp;
-             Weather App
-         </Link>
+          <img
+            alt=""
+            src="/logo.png"
+            width="50"
+            height="50"
+            className="d-inline-block align-center"
+          />&nbsp;
+          Weather App
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Link to="/contact" className="nav-link">
               Contact{' '}
             </Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown 
+              title="Dropdown"
+              id="basic-nav-dropdown"
+              show={dropdownOpen}
+              onClick={toggleDropdown}
+            >
               {list?.map(({ dt }, index) => {
                 return (
-                  <NavDropdown.Item href={getUrl(index)} key={index}>
-                    {moment.unix(dt).format('DD.MM.HH:mm')}{' '}
-                  </NavDropdown.Item>
-
-                  // <Link to={getUrl(index)} className="dropdown-item">
-                  //   {moment.unix(dt).format('DD.MM.HH:mm')}
-                  // </Link>
+                  <Link
+                    to={`/forecast/${index}`}
+                    className="dropdown-item"
+                    key={index}
+                  >
+                    {moment.unix(dt).format('DD.MM.HH:mm')}
+                  </Link>
                 );
               })}
             </NavDropdown>
@@ -54,4 +59,4 @@ function Header({ list }) {
   );
 }
 
-export default Header;
+export default Header; 
